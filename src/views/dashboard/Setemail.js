e747ef5e63e5eabf68a3a43ab49c2b31053fed0e
rutujabopulkar <rutujabopulkar@gmail.com>
2023-08-24 17:45:29 +0530

    second commit

--- src/views/dashboard/Setemail.js
+++ src/views/dashboard/Setemail.js
@@ -1,12 +1,50 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Card, CardBody, Input, Form, Label, FormGroup, Button, FormText } from 'reactstrap'
 
-
 function Setemail() {
+  const initialvalues = { name: "", email: "" }
+  const [formValues, setFormValues] = useState(initialvalues)
+  const [formErrors, setFormErrors] = useState({})
+  const [isSubmit, setIsSubmit] = useState(false)
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setFormValues({ ...formValues, [name]: value })
+  }
+  const validate = (values) => {
+    const errors = {}
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i
+    if (!values.name) {
+      errors.name = "Name is required !"
+    }
+
+    if (!values.email) {
+      errors.email = "Email is required!"
+    } else if (!regex.test(values.email)) {
+      errors.email = "This is not a valid email format!"
+    }
+    return errors
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setFormErrors(validate(formValues))
+    setIsSubmit(true)
+  }
+
+  useEffect(() => {
+    console.log(formErrors)
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      console.log(formValues)
+    }
+  }, [formErrors])
+
   return (
+
     <Card style={{ width: '550px', height: 'auto', padding: '20px', margin: "80px 0 0 180px" }}>
       <CardBody>
-        <Form>
+
+        <Form onSubmit={handleSubmit}>
           <FormText color="dark" className='text-center'>
             <h1>Add your sender information</h1>
           </FormText>
@@ -19,22 +57,25 @@ function Setemail() {
             </Label>
             <Input
               id="exampleText"
+              value={formValues.name}
               name="name"
               type="text"
-              required
+              onChange={handleChange}
             />
+            <p>{formErrors.name}</p>
             <Label for="exampleEmail" style={{ fontSize: "16px", marginTop: '20px' }}>
               Sender email address
             </Label>
             <Input
               id="exampleEmail"
+              value={formValues.email}
               name="email"
-              type="email"
-              required
+              onChange={handleChange}
             />
+            <p>{formErrors.email}</p>
           </FormGroup>
-          <Button color="dark">
-            <a href="/merchant/setemail/checkemail" style={{ color: '#FFF', alignContent: 'center' }} >Continue</a>
+          <Button color="dark" style={{ color: '#FFF', alignContent: 'center' }}>
+            Continue
           </Button>
         </Form>
       </CardBody>
